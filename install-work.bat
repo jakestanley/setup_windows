5a214cd05386a55f2d3af39577d14f2cc3d62681
Jake Stanley <prudentia713@gmail.com>
2016-01-27 09:30:42 +0000

    added node.js to work script

--- install-work.bat
+++ install-work.bat
@@ -1,4 +1,4 @@
-choco install tomcat mysql puush mysql.workbench kdiff3 slack ruby -y
+choco install tomcat mysql puush mysql.workbench kdiff3 slack ruby -y nodejs
 
 :: installs the current 32-bit JDK as well
 choco install jdk -x86 -y
